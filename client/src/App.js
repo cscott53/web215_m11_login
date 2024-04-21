fcd559f7f534fee07ff829164c346297cd19d380
cscott53 <cscott53@email.cpcc.edu>
2024-04-21 10:46:46 -0400

    Added header

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import Header from './Header'
 function App() {
   return (
     <div className="App">
-      {/*  */}
+      <Header/>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
