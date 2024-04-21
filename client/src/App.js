99472d3355056c97be83a6c37267528b48326b94
cscott53 <cscott53@email.cpcc.edu>
2024-04-21 11:45:43 -0400

    Added footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css'
+import Footer from './Footer'
 import Header from './Header'
 function App() {
   return (
     <div className="App">
       <Header/>
+      <main></main>
+      <Footer/>
     </div>
   )
 }
