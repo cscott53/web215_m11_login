f8b9db3badca80be91a3be3f46539aad4798dd67
cscott53 <cscott53@email.cpcc.edu>
2024-04-22 14:10:10 -0400

    Update api.js

--- server/api.js
+++ server/api.js
@@ -9,15 +9,4 @@ const { MongoClient, ServerApiVersion } = require('mongodb'),
       }),
       express = require('express'),
       router = express.Router()
-async function run() {
-  try {
-    await client.connect()
-    console.log(await client.db().admin().listDatabases())
-    // let db = await client.db('admin')
-    // await client.db('admin').command({ ping: 1 })
-    // console.log('Pinged your deployment. You successfully connected to MongoDB!')
-  } finally {
-    await client.close()
-  }
-}
-run().catch(console.dir)
+(async()=>await client.connect())()
