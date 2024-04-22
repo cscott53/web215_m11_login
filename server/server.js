febabc2260f626c622864f0815048d95e0e24eff
cscott53 <cscott53@email.cpcc.edu>
2024-04-22 13:16:00 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,6 @@ const express = require('express'),
       path = require('path'),
       port = process.env.PORT || 3000,
       app = express()
-app.use(express.static(__dirname))
-app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'index.html')))
+app.use(express.static(path.join(__dirname,'../client/build')))
+app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'../client/build/index.html')))
 app.listen(port,()=>console.info(`Server running at port ${port}`))
